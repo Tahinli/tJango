5dd685e882084c9900ba7937e180885e0542a5f6
Tahinli <Kamuflaj@protonmail.com>
2024-06-28 00:56:50 +0300

    feat: :sparkles: rest framework hello world

--- tJango/urls.py
+++ tJango/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path("", include("hello.urls")),
+    path('api-auth/', include('rest_framework.urls')),
+    path('hello', include("hello.urls")),
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
